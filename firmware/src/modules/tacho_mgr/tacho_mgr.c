ad40e21c8980973e4fa195afe36fcd4cc9c7f57b
Karol Stefanik <karol@stefanik.com>
2026-04-20 21:54:17 +0000

    added support  for nucleo_f446ze, improved rpm counting

--- firmware/src/modules/tacho_mgr/tacho_mgr.c
+++ firmware/src/modules/tacho_mgr/tacho_mgr.c
@@ -17,31 +17,44 @@ static const struct pwm_dt_spec tacho_input =
 
 static void tacho_mgr_entry(void) {
   int ret;
+  uint32_t period_ticks, pulse_width_ticks;
+  uint32_t last_valid_rpm = 0;
 
   if (!pwm_is_ready_dt(&tacho_input)) {
     LOG_ERR("Tachometer input device not ready");
     return;
   }
-  uint32_t period_ticks, pulse_width_ticks;
 
   while (1) {
     struct tacho_data current_tacho = {0};
+
+    // Capture the period
     ret = pwm_capture_cycles(tacho_input.dev, tacho_input.channel,
                              PWM_CAPTURE_TYPE_PERIOD, &period_ticks,
                              &pulse_width_ticks, CAPTURE_TIMEOUT);
 
-    if (ret < 0) {
+    if (ret == -EAGAIN || ret == -ETIMEDOUT) {
       current_tacho.rpm = 0;
-      LOG_ERR("Capture error: %d", ret);
+    } else if (ret < 0) {
+      LOG_DBG("Capture failed: %d", ret);
+      current_tacho.rpm = 0;
+    } else {
+      if (period_ticks > 0) {
+        uint32_t raw_rpm =
+            (uint32_t)(MINUTES_TO_USEC / (period_ticks * PULSES_PER_REV));
+
+        // --- GLITCH FILTERING ---
+        if (raw_rpm > 20000) {
+          current_tacho.rpm = last_valid_rpm;
+          LOG_WRN("Discarded glitch: %u RPM", raw_rpm);
+        } else {
+          current_tacho.rpm = raw_rpm;
+          last_valid_rpm = raw_rpm;
+        }
+      }
     }
 
-    current_tacho.rpm = MINUTES_TO_USEC / (period_ticks * PULSES_PER_REV);
-
-    ret = zbus_chan_pub(&rpm_chan, &current_tacho, K_NO_WAIT);
-    if (ret < 0) {
-      LOG_ERR("ZBus  rpm chan publish error: %d", ret);
-    }
-
+    zbus_chan_pub(&rpm_chan, &current_tacho, K_NO_WAIT);
     k_sleep(TACHO_POLL_INTERVAL);
   }
 }
